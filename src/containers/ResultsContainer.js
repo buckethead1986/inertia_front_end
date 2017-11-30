4b03c6d585f9c828cb7e66d57499f02da6af97c0
Nick Paolino <github email address>
2017-11-30 12:11:06 -0500

    setup grids

--- src/containers/ResultsContainer.js
+++ src/containers/ResultsContainer.js
@@ -2,7 +2,18 @@ import React, { Component } from "react";
 
 class ResultsContainer extends Component {
   render() {
-    return <div className="ui container">Results Container</div>;
+    return (
+      <div className="ui right aligned grid">
+        <div className="center aligned two column row">
+          <div className="column">
+            <div className="ui segment">Center aligned row</div>
+          </div>
+          <div className="column">
+            <div className="ui segment">Center aligned row</div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
