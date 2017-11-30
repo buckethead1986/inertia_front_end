73b4da7db94b50f057dd82e32f626538a54fd1df
buckethead1986 <rspeckpfs@gmail.com>
2017-11-30 14:12:48 -0500

    using semantic react library

--- src/components/createChallenge/challenge_form.js
+++ src/components/createChallenge/challenge_form.js
@@ -1,39 +1,97 @@
 import React from "react";
+import SearchDropdown from "./search_dropdown";
 
 class ChallengeForm extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      participants: [],
+      participants: [{ name: "" }],
       spectators: []
     };
   }
 
+  addField = e => {
+    e.preventDefault();
+    this.setState({
+      participants: [...this.state.participants, { name: "" }]
+    });
+  };
+
+  subtractField = e => {
+    e.preventDefault();
+    this.setState({
+      participants: [...this.state.participants]
+    });
+  };
+
   render() {
     return (
       <div>
-        <div className="ui selection dropdown">
-          <input type="hidden" name="challenge_type" />
-          <i className="dropdown icon" />
-          <div className="default text">Challenge Type</div>
-          <div className="menu">
-            <div className="item" data-value="1">
-              Challenge One Person
+        <h1>Make a New Challenge</h1>
+        <div className="ui form">
+          <form
+            onSubmit={e => {
+              console.log("submitted");
+              e.preventDefault();
+              // this.props.handleCreateCocktail(this.state);
+            }}
+          >
+            <SearchDropdown
+              types={[
+                { text: "One on One", value: "One on One" },
+                { text: "Request an opponent", value: "Request an opponent" },
+                { text: "Multi-person Teams", value: "Multi-person Teams" }
+              ]}
+            />
+            <div>
+              Name <input className="inputField" type="text" />
             </div>
-            <div className="item" data-value="0">
-              Challenge Multiple People
+            <div>
+              Description <input type="text" />
             </div>
-          </div>
-        </div>
-        <div>
-          Name <input type="text" />
-        </div>
-        <div>
-          Description <input type="text" />
-        </div>
-        <div>
-          Add Participant <input type="text" />
+            <br />
+            <h3>Add Participants</h3>
+            {this.state.participants.map((participant, i) => (
+              <div key={i} className="fields">
+                <div className="twelve wide field">
+                  <label>Participant Name</label>
+                  <input
+                    name="name"
+                    placeholder="Participant name..."
+                    // onChange={e => this.handleProportionChange(e, i)}
+                  />
+                </div>
+                <div className="ui inline dropdown">
+                  <input type="hidden" name="team" />
+                  <i className="dropdown icon" />
+                  <div className="default text">Select Team</div>
+                  <div className="menu">
+                    <div className="item" data-value="0">
+                      Team A
+                    </div>
+                    <div className="item" data-value="1">
+                      Team B
+                    </div>
+                    <div className="item" data-value="2">
+                      Spectator
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+            <div className="field">
+              <div
+                className="ui basic blue circular icon button"
+                onClick={this.addField}
+              >
+                <i className="plus icon" />
+              </div>
+            </div>
+            <button className="ui large primary button" type="submit">
+              Create Challenge
+            </button>
+          </form>
         </div>
       </div>
     );
