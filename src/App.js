d3dd2f5dc16550b00ee53f2e87b26154dd6e3bf2
buckethead1986 <rspeckpfs@gmail.com>
2017-11-30 12:39:34 -0500

    react routes working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
-import "semantic-ui-css/semantic.min.css";
-
+import Challenge from "./components/createChallenge/challenge";
 import InertiaContainer from "./containers/InertiaContainer";
+const NewChallenge = () => <Challenge />;
+const Inertia = () => <InertiaContainer />;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <InertiaContainer />
+      <div>
+        <Router>
+          <div>
+            <Route exact path="/challenge/new" component={NewChallenge} />
+            <Route exact path="/inertia" component={Inertia} />
+          </div>
+        </Router>
       </div>
     );
   }
