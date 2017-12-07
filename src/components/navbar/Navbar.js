04d499a7c796c6357613822dad3b6889c74a7793
buckethead1986 <rspeckpfs@gmail.com>
2017-12-07 13:09:52 -0500

    adding user profile page

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,49 +1,30 @@
 import React, { Component } from "react";
 import { Menu } from "semantic-ui-react";
 
-export default class MenuExampleBasic extends Component {
-  state = {};
+const Navbar = props => {
+  return (
+    <Menu>
+      <Menu.Item name="My Profile" onClick={props.profileLink}>
+        My Profile
+      </Menu.Item>
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+      <Menu.Item name="Challenges" onClick={props.challengesLink}>
+        Challenges
+      </Menu.Item>
 
-  render() {
-    const { activeItem } = this.state;
+      <Menu.Item name="Users" onClick={props.usersLink}>
+        Users
+      </Menu.Item>
 
-    return (
-      <Menu>
-        <Menu.Item
-          name="Challenges"
-          active={activeItem === "Challenges"}
-          onClick={this.props.challengesLink}
-        >
-          Challenges
-        </Menu.Item>
+      <Menu.Item name="New Challenge" onClick={props.newChallengeLink}>
+        New Challenge
+      </Menu.Item>
 
-        <Menu.Item
-          name="Users"
-          active={activeItem === "Users"}
-          onClick={this.props.usersLink}
-        >
-          Users
-        </Menu.Item>
+      <Menu.Item position="right" name="Sign Out" onClick={props.logout}>
+        Sign Out
+      </Menu.Item>
+    </Menu>
+  );
+};
 
-        <Menu.Item
-          name="New Challenge"
-          active={activeItem === "New Challenge"}
-          onClick={this.props.newChallengeLink}
-        >
-          New Challenge
-        </Menu.Item>
-
-        <Menu.Item
-          position="right"
-          name="Sign Out"
-          active={activeItem === "Sign Out"}
-          onClick={this.props.logout}
-        >
-          Sign Out
-        </Menu.Item>
-      </Menu>
-    );
-  }
-}
+export default Navbar;
